6c4c8b68d0edd9d732e9759da1a054ba768c0df7
Badis Saidani <saidanii.badis@gmail.com>
2024-11-26 00:00:32 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// Init
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+// Middleware
+app.use(cors());
+
+// Route
+app.get("/api/message", (req, res) => {
+	res.json({message: "Badis Saidani" });
+});
+
+
+// Start the serve
+const PORT = 3000;
+app.listen(PORT, () => {
+	console.log(`Backend server is running on port ${PORT}`);
+});
